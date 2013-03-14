3957a98d48580db309ac6b70cff4c6579b11153b
ranjeet <hackmerko@gmail.com>
2013-03-14 00:19:08 -0700

    to add two numbers

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,add;
+printf("enter the first number:");
+scanf("%d",&a);
+printf("enter the second number:");
+scanf("%d",&b);
+add=a+b;
+printf("addition of number is= %d\n",add);
+return 0;
+}
+
