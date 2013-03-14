9ea2c5a8006cd6379f8251f5234bebaa3f74e3f8
ranjeet <hackmerko@gmail.com>
2013-03-13 23:36:09 -0700

    to check even odd number

---
+++ even.c
@@ -0,0 +1,23 @@
+
+/* to check weather number is even or odd*/
+
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a&1==1)
+printf("number is odd\n");
+else
+printf("number is even\n");
+return 0;
+}
+
+
+
+
+
+
+
+
