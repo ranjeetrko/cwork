d9e35ffde4f59241a226ad54fe61381115ec2e78
rnjeetrko <ranjeetrko@gmail.com>
2013-03-19 00:32:11 -0700

    to print star

---
+++ star.c
@@ -0,0 +1,17 @@
+/*print star pattern*/
+#include<stdio.h>
+int main()
+{
+int i,j,num;
+printf("enter the number=");
+scanf("%d",&num);
+for(i=0;i<num;i++)
+{
+for(j=0;j<=i;j++)
+{
+printf("*");
+}
+printf("\n");
+}
+return 0;
+}
